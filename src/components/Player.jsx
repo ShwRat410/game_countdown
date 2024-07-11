7edcafc819026762ee1370fad7d37668f1c7e515
shweta255178 <shwetamds333@gmail.com>
2024-07-11 17:32:47 +0100

    Managing values with refs - two way binding

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,22 +1,20 @@
+import { useRef } from "react";
 import { useState } from "react";
 
 export default function Player() {
-  const [enteredPlayerName,setEnteredPlayerName] = useState('');
-  const [submitted,setSubmitted] = useState(false);
-  function handleOnChange(event){
-    setSubmitted(false)
-    setEnteredPlayerName(event.target.value)
-  }
+  const [enteredPlayerName,setEnteredPlayerName] = useState(null);
+  const playerName = useRef();
+
   function handleClick() {
-    setSubmitted(true)
+    setEnteredPlayerName(playerName.current.value)
   }
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredPlayerName : 'unknown entity'}</h2>
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" 
-        onChange={handleOnChange}
-        value={enteredPlayerName}
+        <input 
+        type="text"
+        ref={playerName} 
         />
         <button onClick={handleClick}>Set Name</button>
       </p>
