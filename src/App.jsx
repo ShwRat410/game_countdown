b697260572e8d8afa267e0ecb5f071ee4d68885c
shweta255178 <shwetamds333@gmail.com>
2024-07-11 18:35:57 +0100

    Adding challenges

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Easy" targetTime={5}></TimerChallenge>
+        <TimerChallenge title="Not Easy" targetTime={5}></TimerChallenge>
+        <TimerChallenge title="Getting Tough" targetTime={5}></TimerChallenge>
+        <TimerChallenge title="Pros Only" targetTime={5}></TimerChallenge>
+      </div>
     </>
   );
 }
