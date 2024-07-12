b153f0778dd2d3365c029d1efc5c1a6c58268572
shweta255178 <shwetamds333@gmail.com>
2024-07-12 16:27:10 +0100

    Modal component+forwarding refs

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,14 +1,17 @@
 import React,{useRef, useState} from 'react'
+import ResultModal from './ResultModal';
 
 export default function TimerChallenge({title,targetTime}) {
 
   const [timerExpired,setTimerExpired]=useState(false)
   const [timerStarted,setTimerStarted]=useState(false)
   const timer = useRef()
+  const dialog = useRef()
 
   function handleStart(){
     timer.current=setTimeout(() => {
         setTimerExpired(true);
+        dialog.current.showModal();
       }, targetTime*1000); 
       setTimerStarted(true)
     }
@@ -17,6 +20,8 @@ export default function TimerChallenge({title,targetTime}) {
         clearTimeout(timer.current)
     }
   return (
+    <>
+    <ResultModal ref={dialog} result="lost" targetTime={targetTime}></ResultModal>
     <section className="challenge">
       <h2>{title}</h2>
       <p>{timerExpired && "You lost"}</p>
@@ -24,5 +29,6 @@ export default function TimerChallenge({title,targetTime}) {
       <p><button onClick={timerStarted?handleStop:handleStart}>{timerStarted ? "Stop challenge" : "Start challenge"}</button></p>
       <p className={timerStarted?'active':undefined}>{timerStarted && !timerExpired ? "Timer is running" : "Timer inactive"}</p>
     </section>
+    </>
   )
 }
