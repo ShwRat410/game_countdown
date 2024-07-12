f572b747a2206bd9885d806d2c77a4e0ef1d7fa4
shweta255178 <shwetamds333@gmail.com>
2024-07-12 17:53:33 +0100

    Using useImperativeHandle for exposing componnent API

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,7 +1,15 @@
-import React, { forwardRef } from 'react'
+import React, { forwardRef, useImperativeHandle, useRef } from 'react'
 const ResultModal= forwardRef(function ResultModal({result,targetTime},ref) {
+    const dialog = useRef()
+    useImperativeHandle(ref,()=>{
+        return{
+            open(){
+                dialog.current.showModal()
+            }
+        }
+    })
   return (
-      <dialog className='result-modal' ref={ref}>
+      <dialog className='result-modal' ref={dialog}>
         <h1>You {result}</h1>
         <p>The target time was <strong>{targetTime} seconds</strong></p>
         <p>You stopped the timer with <strong>x seconds left</strong></p>
